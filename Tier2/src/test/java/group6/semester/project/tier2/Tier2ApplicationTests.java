bcbbad37e57ac7b36ad1d9132490f75787ba5af2
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-04-21 17:23:10 +0200

    T2 : Set all the classses and proto files for T2 server. (By Sachin)

---
+++ Tier2/src/test/java/group6/semester/project/tier2/Tier2ApplicationTests.java
@@ -0,0 +1,13 @@
+package group6.semester.project.tier2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tier2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
