7e1f054763fc9b5e80dd4e90be8c11a63ed5b975
Himal sharma <himal28924@gmail.com>
2022-04-30 11:16:42 +0200

    SEP3-31 User proto and other things done and fix

--- Tier2/src/main/java/group6/semester/project/controller/UserController.java
+++ Tier2/src/main/java/group6/semester/project/controller/UserController.java
@@ -5,6 +5,7 @@ import group6.semester.project.model.User;
 
 import group6.semester.project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +14,38 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class UserController {
 
-    @Autowired
+
     private UserService userService;
+
+
+   public UserController(UserService userService)
+    {
+     this.userService = userService;
+     }
 
    @PostMapping(value = "/user")
     public ResponseEntity addUser(@RequestBody User user) throws Exception {
         try{
-            ResponseEntity message = userService.CreateUserAsync(user);
-            return ResponseEntity.ok().body(message);
+            User addedUser = userService.CreateUserAsync(user);
+            return ResponseEntity.ok().body(addedUser);
         }
         catch (Exception e){
             throw new Exception(e.getMessage());
         }
    }
+
+
+  @GetMapping(value = "/user/{username}")
+  @ResponseBody
+  public ResponseEntity getMessage(@PathVariable String username){
+    try {
+
+      User userFromServer = userService.GetUserAsync(username);
+      return ResponseEntity.ok(userFromServer);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 
 }
