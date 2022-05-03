6118e247d73635702b0a929801d5ef09bb541249
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-03 12:22:51 +0200

    Added entity classes ...

---
+++ Tier3/Entities/Models/Subcategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models; 
+
+public class Subcategory {
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Post> Posts { get; set; }
+    public Category Category { get; set; }
+}
--- Tier3/Entities/Models/User.cs
+++ Tier3/Entities/Models/User.cs
@@ -10,4 +10,7 @@ public class User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? Role { get; set; }
+
+    public ICollection<Post>? Posts  { get; set; }
+
 }
