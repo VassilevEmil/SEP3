1c9a362abab460dae79be1b9584395cb0d3fb9e5
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-04 12:23:24 +0200

    Added the proto for now... feel free to change them as needed

---
+++ Tier2/src/main/java/GRPCService/PostGrpc.java
@@ -0,0 +1,205 @@
+package GRPCService;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.15.0)",
+    comments = "Source: post.proto")
+public final class PostGrpc {
+
+  private PostGrpc() {}
+
+  public static final String SERVICE_NAME = "post.Post";
+
+  // Static method descriptors that strictly reflect the proto.
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static PostStub newStub(io.grpc.Channel channel) {
+    return new PostStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static PostBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new PostBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static PostFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new PostFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class PostImplBase implements io.grpc.BindableService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class PostStub extends io.grpc.stub.AbstractStub<PostStub> {
+    private PostStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private PostStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected PostStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new PostStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class PostBlockingStub extends io.grpc.stub.AbstractStub<PostBlockingStub> {
+    private PostBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private PostBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected PostBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new PostBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class PostFutureStub extends io.grpc.stub.AbstractStub<PostFutureStub> {
+    private PostFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private PostFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected PostFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new PostFutureStub(channel, callOptions);
+    }
+  }
+
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final PostImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(PostImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class PostBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    PostBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return GRPCService.PostOuterClass.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("Post");
+    }
+  }
+
+  private static final class PostFileDescriptorSupplier
+      extends PostBaseDescriptorSupplier {
+    PostFileDescriptorSupplier() {}
+  }
+
+  private static final class PostMethodDescriptorSupplier
+      extends PostBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    PostMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (PostGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new PostFileDescriptorSupplier())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
