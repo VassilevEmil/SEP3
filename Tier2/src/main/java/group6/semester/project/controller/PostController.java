e637bee52349eab54b4fcb3c376e5e34288eb94f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-05 18:39:18 +0200

    Added PostController.java

--- Tier2/src/main/java/group6/semester/project/controller/PostController.java
+++ Tier2/src/main/java/group6/semester/project/controller/PostController.java
@@ -1,7 +1,10 @@
 package group6.semester.project.controller;
 
+import group6.semester.project.model.Post;
 import group6.semester.project.services.PostService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostController {
@@ -11,6 +14,21 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
+
+    @PostMapping(value = "/post{subForumId:int}")
+
+    public ResponseEntity addPost(@RequestBody Post post, @PathVariable int subForumId){
+
+        try {
+            Post addedPost = postService.addPost(post, subForumId);
+            return ResponseEntity.ok(addedPost);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+    }
+
 
 
 }
