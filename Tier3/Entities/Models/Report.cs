d52cdcfbc5d9f5c484e636b78dfd3c4dadf63d52
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-14 22:43:22 +0200

    Added bookmarks and stuffs

---
+++ Tier3/Entities/Models/Report.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models; 
+
+public class Report 
+
+{
+
+    [ForeignKey("Post")]
+    public int PostId { get; set; }
+
+    [ForeignKey("User")]
+    public int ReporterUsername { get; set; }
+    public string Reason { get; set; }
+}
