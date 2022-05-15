aaecf90d03450a81bb87e8e69195f192e6c4c129
Adrian <militaruadriancrisitna@gmail.com>
2022-05-15 15:20:04 +0200

    SEP3-69 add REST client/server

---
+++ Tier2/src/main/java/group6/semester/project/controller/BookmarkController.java
@@ -0,0 +1,54 @@
+package group6.semester.project.controller;
+
+import group6.semester.project.model.Bookmark;
+import group6.semester.project.model.Post;
+import group6.semester.project.services.BookmarkService;
+import group6.semester.project.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService =bookmarkService;
+    }
+
+    @PostMapping(value = "/bookmark")
+    public ResponseEntity addPost(@RequestBody Bookmark bookmark) {
+        try {
+            bookmarkService.AddBookmark(bookmark);
+            return ResponseEntity.ok("Bookmark added:" + bookmark.getPost().getId() + " by " + bookmark.getUser().getUsername());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @DeleteMapping(value = "/bookmark/{postId}/{userName}")
+    public ResponseEntity addPost(@PathVariable int postId, @PathVariable String userName) {
+        try {
+            bookmarkService.RemoveBookmark(postId,userName);
+            return ResponseEntity.ok("Bookmark removed:" + postId + userName);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping(value = "/bookmark/all/{current}")
+    public ResponseEntity<List<Post>> getAllPosts(@PathVariable int current) throws RuntimeException {
+
+        System.out.println("Get all posts is called for bookmark");
+        List<Post> posts = bookmarkService.GetAllPostsByBookMark(current);
+        return ResponseEntity.ok(posts);
+
+    }
+
+}
