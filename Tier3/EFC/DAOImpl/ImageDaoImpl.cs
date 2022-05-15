a2324fac5a4ea91971bad62822ff99f7281430d2
Himal sharma <41054866+himal28924@users.noreply.github.com>
2022-05-15 14:25:52 +0200

    SEP3-50 trying to add efc for

--- Tier3/EFC/DAOImpl/ImageDaoImpl.cs
+++ Tier3/EFC/DAOImpl/ImageDaoImpl.cs
@@ -15,40 +15,42 @@ public class ImageDaoImpl : IImageService
     }
 
 
-    public async Task<bool> AddImage(int postId, string fileType, byte[] file)
+    public async Task<bool> AddImage(int postId,string fileType, byte[] file)
     {
         Image image = new Image();
-        image.PostId = postId;
-        image.Href=String.Empty;
-        var returnImage =  _context.Images.AddAsync(image).Result.Entity;
-        await _context.SaveChangesAsync();
-        Console.WriteLine(returnImage.Id.ToString() + "Id of returned image");
-        var href = returnImage.Id + "." + fileType;
-        if (ByteArrayToFile(href, file))
-        {
-            returnImage.Href = href;
-            await _context.SaveChangesAsync();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+         var post= _context.Posts.FirstOrDefaultAsync(post => postId.Equals(post.Id)).Result;
+            post.Images.Add(image);
+            
+            var a =  _context.Posts.Update(post);
 
+            string filename =  a.Entity.Images.Count+"."+fileType;
 
-    private bool ByteArrayToFile(string fileName, byte[] byteArray)
-    {
-        try
-        {
-            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            if (ByteArrayToFile(filename, file))
             {
+                image.Id = Convert.ToInt32(filename);
+                image.Href = filename;
+                var imageDb = _context.Images.Update(image);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+    }
+    
+    
+    private bool ByteArrayToFile(string fileName, byte[] byteArray) {
+        try {
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
                 fs.Write(byteArray, 0, byteArray.Length);
                 return true;
             }
         }
-        catch (Exception ex)
-        {
+        catch (Exception ex) {
             Console.WriteLine("Exception caught in process: {0}", ex);
             return false;
         }
