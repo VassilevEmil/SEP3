2cc2f43480cca98dd49ef9e79530a7f562fd4441
Adrian <militaruadriancrisitna@gmail.com>
2022-05-15 15:20:04 +0200

    SEP3-69 add REST client/server

---
+++ Tier1/Tier1/HttpServices/BookmarkHttpClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Contracts;
+using Entities.Models;
+
+namespace HttpServices;
+
+public class BookmarkHttpClient : IBookmarkService
+{
+    
+    
+    private T GetDeserialized<T>(string jsonFormat) {
+        T obj = JsonSerializer.Deserialize<T>(jsonFormat, new JsonSerializerOptions() {
+            PropertyNameCaseInsensitive = true
+        }) !;
+        return obj;
+    }
+
+    public async Task AddBookmark(Bookmark bookmark)
+    {
+        try {
+            var content = await ClientAPI.getContent(Methods.Post, $"/bookmark", bookmark);
+        }
+        catch (Exception e) {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task RemoveBookmark(Bookmark bookmark)
+    {
+        try {
+            var content = await ClientAPI.getContent(Methods.Delete, $"/bookmark/{bookmark.Post.Id}/{bookmark.User.Username}");
+        }
+        catch (Exception e) {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<List<Post>> GetAllPostsByBookMark(int current)
+    {
+        try
+        {
+            string client = await ClientAPI.getContent(Methods.Get, $"/bookmark/all/{current}");
+            List<Post> list = GetDeserialized<List<Post>>(client);
+            return list;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
